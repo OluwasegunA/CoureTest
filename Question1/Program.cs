11f1fa07b8159dcc537b9aaec6a7c993acbad552
Oluwasegun Ariyo <oluwasegunayobami7@gmail.com>
2022-02-21 16:53:05 +0100

    CoureTest

---
+++ Question1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program{
+  public Program(){}
+  public static int totalScore(int[] arr){
+    int result = 0;
+
+    for(int i =0; i< arr.Length; i++){
+      result = arr[i] %2 ==0 ? result +1: result+3;
+    }
+
+    for(int i =0; i< arr.Length; i++){
+      if(arr[i] ==8){
+        result = result + 5;
+      }
+    }
+
+    Console.WriteLine($"Total Score for the array is: {result}");
+
+    return result;
+  }
+
+  public static void Main(string[] args){
+    int [] arr = {1,2,3,4,5};
+    int [] arr1 = {15,25,35};
+    int [] arr2 = {8,8};
+    var result = Program.totalScore(arr2);
+
+    Console.WriteLine(result);
+  }
+}
